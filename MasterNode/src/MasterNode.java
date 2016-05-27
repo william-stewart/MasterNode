64ace135bb1e27d0d2d1d2565f189e47cfe62ec3
wstewart <william.stewart@furman.edu>
2016-05-27 14:49:20 -0400

    Trying to get debug and check whether it is recieving answer from workers

--- MasterNode/src/MasterNode.java
+++ MasterNode/src/MasterNode.java
@@ -11,6 +11,7 @@ import java.net.Socket;
 public class MasterNode {
 	static String clientSentence;
 	static String capitalizedSentence;
+	static String workerSentence = "already set";
 	
 	public static void receiveAndReturn() throws Exception{
 		ServerSocket welcomeSocket = new ServerSocket(6789);
@@ -20,25 +21,25 @@ public class MasterNode {
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 			clientSentence = inFromClient.readLine();
-			String workerSentence = sendToWorker(clientSentence);
+			sendToWorker(clientSentence);
 			capitalizedSentence = clientSentence.toUpperCase() + '\n';
 			outToClient.writeBytes(workerSentence);
 		}
 	}
 	
-	public static String sendToWorker(String input) throws Exception{
+	public static void sendToWorker(String input) throws Exception{
 		DatagramSocket clientSocket = new DatagramSocket();
-		InetAddress IPAddress = InetAddress.getByName("10.102.55.23");
+		InetAddress IPAddress = InetAddress.getByName("156.143.90.9");
 		byte[] sendData = new byte[1024];
 		byte[] receiveData = new byte[1024];
 		sendData = input.getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,IPAddress,9876);
+		DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,IPAddress,9875);
 		clientSocket.send(sendPacket);
 		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 		clientSocket.receive(receivePacket);
 		String output = new String(receivePacket.getData());
 		clientSocket.close();
-		return output;
+		workerSentence = output;
 	}
 
 	public static void main(String[] args) {
